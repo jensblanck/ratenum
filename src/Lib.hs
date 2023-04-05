f3ce69a9bfa6b2a83f6d6aa768356ee8c1a5f2a1
Jens Blanck <jens.blanck@gmail.com>
2023-04-05 20:47:44 +0100

    renamed diagonal and gcd versions

--- src/Lib.hs
+++ src/Lib.hs
@@ -12,16 +12,20 @@ import Prelude hiding (gcd)
 import Data.Ratio
 import Data.List (group)
 
+-- * Enumerating rationals by diagonals
+
 -- | List of positive rational numbers created from diagonals. Contains infinitely many duplicates. Merely here for comparison.
-rat1 :: [Rational]
-rat1 = concat (diags [[m % n | m <- [1..]] | n <- [1..]])
+ratPosDiag1 :: [Rational]
+ratPosDiag1 = concat (diags [[m % n | m <- [1..]] | n <- [1..]])
   where diags = diags' []
         diags' _ [] = error "Impossible"
         diags' xss (ys : yss) = map head xss : diags' (ys : map tail xss) yss
 
 -- | List of positive rational numbers created from diagonals. Although diaganals now constructed directly. Contains infinitely many duplicates. Merely here for comparison.
-rat2 :: [Rational]
-rat2 = concat [[m % (d-m) | m <- [1..d-1]] | d <- [2..]]
+ratPosDiag2 :: [Rational]
+ratPosDiag2 = concat [[m % (d-m) | m <- [1..d-1]] | d <- [2..]]
+
+-- * Euclid's gcd algorithm
 
 gcd :: Integer -> Integer -> Integer
 gcd m n
@@ -30,30 +34,39 @@ gcd m n
   | m == n = m
   | otherwise = error "Not possible"
 
-igcd :: Integer -> Integer -> (Integer, [Bool])
-igcd m n
-  | m < n = step False $ igcd m (n-m)
-  | m > n = step True $ igcd (m-n) n
+-- | Computes the gcd of two positive integers (no checks done) paired
+-- with a trace of the computation. Uses subtractive Euclid algorithm.
+gcdTrace' :: Integer -> Integer -> (Integer, [Bool])
+gcdTrace' m n
+  | m < n = step False $ gcdTrace' m (n-m)
+  | m > n = step True $ gcdTrace' (m-n) n
   | m == n = (m, [])
   | otherwise = error "Not possible"
   where step b (m', bs) = (m', b:bs)
 
-pgcd :: Integer -> Integer -> [Bool]
-pgcd m n = snd $ igcd m n
+-- | Returns the trace of subtractive Euclid algorithm on two positive
+-- integers.
+gcdTrace :: Integer -> Integer -> [Bool]
+gcdTrace m n = snd $ gcdTrace' m n
 
-ungcd :: Integer -> [Bool] -> (Integer, Integer)
-ungcd d bs = foldr undo (d,d) bs
+-- | Takes the gcd and a trace of subtractive Euclid algorithm to
+-- reconstruct pair of integers.
+gcdTraceToPair :: Integer -> [Bool] -> (Integer, Integer)
+gcdTraceToPair d bs = foldr undo (d,d) bs
   where undo False (m, n) = (m, n + m)
         undo True (m, n) = (m + n, n)
 
-rat3 :: [Rational]
-rat3 = map (uncurry (%) . ungcd 1) boolseq
+-- | List of positive rational numbers without duplicates. Maps
+-- 'gcdTraceToPair' on all possible Boolean sequences, i.e., all
+-- possible subtractive Euclid traces. Setting the gcd to 1 gives
+-- reduced fractions. This is @rat3@ in Gibbins et al. paper.
+ratPosGcd :: [Rational]
+ratPosGcd = map (uncurry (%) . gcdTraceToPair 1) boolseq
+  where boolseq = [] : [b : bs | bs <- boolseq, b <- [False, True]]
 
-boolseq :: [[Bool]]
-boolseq = [] : [b : bs | bs <- boolseq, b <- [False, True]]
+-- * Tree data type
 
--- Stern-Brocot
-
+-- | Infinite trees over arbitrary type. There are no leaves.
 data Tree a = Node (Tree a) a (Tree a)
 
 instance Foldable Tree
@@ -68,6 +81,8 @@ treeLevels (Node l k r) = [k] : zipWith (<>) (treeLevels l) (treeLevels r)
 unfoldt :: (t -> (a, t, t)) -> t -> Tree a
 unfoldt f x = let (a,y,z) = f x in Node (unfoldt f y) a (unfoldt f z)
         
+-- * Stern-Brocot
+
 stern_brocot :: Tree Rational
 stern_brocot = fmap (uncurry (%)) (unfoldt step ((0,1),(1,0)))
   where step (l,r) = let m = adj l r
